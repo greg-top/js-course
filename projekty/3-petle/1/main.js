d91065b5c529632da694f0e795ae9940865fa74b
gregory_sf9 <greg@studioforty9.com>
2019-02-14 16:01:19 +0000

    Proejkt 3.1 - rozwiazanie

--- projekty/3-petle/1/main.js
+++ projekty/3-petle/1/main.js
@@ -1,1 +1,26 @@
 // tutaj rozwiązanie
+
+const btn = document.querySelector("button");
+let firstFlag = true;
+let fontSize = 10;
+
+const liNodeList = document.querySelectorAll("li");
+const liArray = [...liNodeList];
+
+
+btn.addEventListener("click", function () {
+    // console.log("test");
+
+    if (firstFlag) {
+        for (let i = 0; i < liArray.length; i++) {
+            liArray[i].style.display = "list-item";
+            liArray[i].style.fontSize = `${fontSize}px`;
+        }
+        firstFlag = !firstFlag;
+    } else {
+        fontSize += 1;
+        for (let i = 0; i < liArray.length; i++) {
+            liArray[i].style.fontSize = `${fontSize}px`;
+        }
+    }
+});
