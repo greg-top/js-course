ce216fc5dfbc37fc3025bf67229cc9872b71fa97
gregory_sf9 <greg@studioforty9.com>
2019-08-27 12:13:26 +0100

    Chapter 6.1

--- projekty/6-ProjektLosowanie/6.1-losowanieImieniaDziecka/main.js
+++ projekty/6-ProjektLosowanie/6.1-losowanieImieniaDziecka/main.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('button');
 const names = ['Jagienka', 'Dobrawa (Dobrusia)', 'Daria', 'Asia', 'Kira', 'Marysia', 'Anastazja'];
+const greetings = ['Uważam, że ', 'Genialne imię to ', 'Zastanów się nad ', 'A co ty na imię ', 'Piękne imię to '];
 const div = document.querySelector('div');
 
 //losowość 
@@ -9,7 +10,8 @@ const div = document.querySelector('div');
 //console.log(Math.floor(Math.random() * 7)) //metoda floow odrzuca część ułamkową, wypadnie wynik między 0-6
 const nameGenerator = () => {
     const nameIndex = Math.floor(Math.random() * names.length); //sprawdzenie dlugości tablicy
-    div.textContent = `Świetnym imieniem będzie ${names[nameIndex]}`;
+    const greetingIndex = Math.floor(Math.random() * greetings.length);
+    div.textContent = `${greetings[greetingIndex]} ${names[nameIndex]}`;
 }
 
 btn.addEventListener('click', nameGenerator);
