c7c946d65d25021a4bd0c1d6b76ac625e693f9bc
gregory_sf9 <greg@studioforty9.com>
2019-10-08 16:17:02 +0100

    Chapter 9 - fixed slider functionality

--- projekty/9-slider/main.js
+++ projekty/9-slider/main.js
@@ -1,31 +1,42 @@
+//------------------
+// ----Interfejs----
+//------------------
+// tablica przechowująca obiekty opisujące poszczególne slajdy (ścieżka do obrazu + opis slajdu)
 const slideList = [{
- img: "images/img1.jpg",
- text: 'Pierwszy tekst'
-},
-{
- img: "images/img2.jpg",
- text: 'Drugi tekst'
-},
-{
- img: "images/img3.jpg",
- text: 'Trzeci tekst'
-}];
+        img: 'images/img1.jpg',
+        text: 'Pierwszy tekst'
+    },
+    {
+        img: 'images/img2.jpg',
+        text: 'Drugi tekst'
+    },
+    {
+        img: 'images/img3.jpg',
+        text: 'Trzeci tekst'
+    }
+];
 
-const image = document.querySelector('img.slider');
-const h1 = document.querySelector('h1.slider');
-// Interfejs
-const time = 3000;
-let active = 0;
+//obraz slajsu
+const slideImg = document.querySelector('img.slider');
+const slideText = document.querySelector('h1.slider');
+
+// czas zmiany slajdu
+const time = 2000;
+//aktywny slajd
+let activeSlide = 0;
+
+//------------------
+//--Implementacja---
+//------------------
 
 const changeSlide = () => {
- active++;
- if (active === slideList.length) {
-  active = 0;
- }
- image.src = slideList[active].img;
- h1.textContent = slideList[active].text;
+    activeSlide++;
+    if (activeSlide === slideList.length) {
+        activeSlide = 0;
+    }
+    slideImg.src = slideList[activeSlide].img;
+    slideText.textContent = slideList[activeSlide].text;
 
 }
-setInterval(changeSlide, time)
 
-// Implementacje
+setInterval(changeSlide, time);
