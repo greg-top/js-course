3a1b2bd2ebcdadab75458f99a4e1db9313bfe9ae
gregory_sf9 <greg@studioforty9.com>
2019-12-03 12:40:17 +0000

    Pojekt 9 - slider - zmiana atrybutu active dla kropek slidera

--- projekty/9-slider/main.js
+++ projekty/9-slider/main.js
@@ -1,27 +1,30 @@
 //------------------
 // ----Interfejs----
 //------------------
-// tablica przechowująca obiekty opisujące poszczególne slajdy (ścieżka do obrazu + opis slajdu)
+// tablica przechowująca obiekty opisujące poszczególne slides (ścieżka do obrazu + opis slajdu)
 const slideList = [{
         img: 'images/img1.jpg',
-        text: 'Pierwszy tekst'
+        text: 'Pierwszy tekst',
+        positionId: '#one'
     },
     {
         img: 'images/img2.jpg',
-        text: 'Drugi tekst'
+        text: 'Drugi tekst',
+        positionId: '#two'
     },
     {
         img: 'images/img3.jpg',
-        text: 'Trzeci tekst'
+        text: 'Trzeci tekst',
+        positionId: '#three'
     }
 ];
 
-//obraz slajsu
+//właściwości slide
 const slideImg = document.querySelector('img.slider');
 const slideText = document.querySelector('h1.slider');
-
-// czas zmiany slajdu
-const time = 2000;
+const slideDots = [...document.querySelectorAll('.dots span')];
+// czas zmiany slide
+const time = 4000;
 //aktywny slajd
 let activeSlide = 0;
 
@@ -36,7 +39,18 @@ const changeSlide = () => {
     }
     slideImg.src = slideList[activeSlide].img;
     slideText.textContent = slideList[activeSlide].text;
+    changeDot();
+}
 
+const changeDot = () => {
+    //reset active slide dot
+    // console.log(slideDots);
+    slideDots.forEach((dot) => {
+        if (dot.classList.contains('active')) {
+            dot.classList.remove('active');
+        }
+    })
+    document.querySelector(slideList[activeSlide].positionId).classList.add('active');
 }
 
 setInterval(changeSlide, time);
