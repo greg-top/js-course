6d986ff59a66eefddb6ef55f5f3323c4cff8d579
gregory_sf9 <greg@studioforty9.com>
2019-12-13 17:02:22 +0000

    Closure - licznik

---
+++ projekty/10-ClosuresLicznikICzasomierz/10.2-CounterZClosure/main.js
@@ -0,0 +1,33 @@
+// Closure (domknięcie) to mechanizm, który pomaga nam uzyskać dostęp do danej wartości poza funkcją i gdy ta funkcja nie jest już aktywna.
+
+// przyklad:
+// function x() {
+//     let number = 3;
+
+//     function y() {
+//         console.log(number);
+//     }
+//     return y;
+// }
+
+// const example = x();
+
+// example();
+
+// Closure - zmienna istnieje w innej funkcji, mimo, że funkcja, w której była stworzona ta zmienna nie jest już aktywna. 
+
+//W pamieci programu zmienna istnieje jeśli prowadzi do niej referencja z innej funkcji. Mozemy mieć do niej dostęp, ale także je zmieniać. 
+
+const add = (start = 0) => {
+    let number = start;
+    return () => {
+        number++;
+        document.body.textContent = `aktualna wartosc licznika to ${number}`;
+    }
+}
+
+// const counter = add();
+const counterFrom5 = add(5)
+
+// document.addEventListener('click', counter);
+document.addEventListener('click', counterFrom5);
