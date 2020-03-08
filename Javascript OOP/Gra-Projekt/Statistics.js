44f475db2d797436c4960a5f23c4a3addb41ede0
gregory_sf9 <greg@studioforty9.com>
2020-03-08 16:06:43 +0000

    Gra w OOP

---
+++ Javascript OOP/Gra-Projekt/Statistics.js
@@ -0,0 +1,53 @@
+class Statistics {
+    constructor() {
+        this.gameResults = [];
+        //przykładowe wartości właściowści gameResults
+        // this.gameResults = [{
+        //         win: true,
+        //         bid: 2
+        //     },
+        //     {
+        //         win: false,
+        //         bid: -3
+        //     }
+        // ];
+    }
+    // wszystkie metody mogą zostać utworzone w prototypie ponieważ korzystamy w nich z publicznej właściwości gameResults;
+
+    //dodaj informacje z tury gry
+    addGameToStatistics(win, bid) {
+        // let gameResult = {
+        //     win: win,
+        //     bid: bid
+        // }
+        // szybszy zapis powyższej metody
+        let gameResult = {
+            win,
+            bid
+        }
+        console.log(gameResult);
+        this.gameResults.push(gameResult);
+    }
+
+    //metoda ma zwrócić wartość właściwości gameResults
+    showGameStatistics() {
+        let games = this.gameResults.length;
+        let wins = this.gameResults.filter(result => result.win).length; //zwraca nową tablicę gdzie wszystkie elementy zwracają true, następnie za pomocą metody length zwracamy ilość elementów w nowo utworzonej tablicy
+        let losses = this.gameResults.filter(result => !result.win).length;
+
+        console.log(`gier: ${games}, wygranych: ${wins}, przegranych: ${losses}`);
+
+
+        return [games, wins, losses]
+        // return liczbaGier, liczbaWygranych, liczbaPorażek
+
+    }
+}
+
+// const stats = new Statistics();
+// stats.addGameToStatistics(true, 2);
+// stats.addGameToStatistics(false, -10);
+// stats.addGameToStatistics(false, -6);
+// stats.gameResults;
+
+// stats.showGameStatistics();
---
+++ Javascript OOP/Gra-Projekt/Wallet.js
@@ -0,0 +1,45 @@
+class Wallet {
+    constructor(money) {
+        let _money = money;
+
+        //Wszystkie metody muszą zostać utworzone w konstruktorze aby mieć dostęp do prywatnej właściwości _money
+        //pobieranie aktualnej zawartoßci portfela
+        this.getWalletValue = () => _money;
+        // To samo ponizej
+        // this.getWalletValue = () => {
+        //     return _money;
+        // }
+
+        //Sprawdzanie czy użytkownik ma odpowiednią ilość środków
+        this.checkCanPlay = value => {
+            if (_money >= value) return true; //po spełnieniu warunku metoda zakończy swoje działanie w tej lini kodu
+            return false;
+        }
+
+        //zmiana obiektu wallet po rundzie gry
+        this.changeWallet = (value, type = "+") => {
+            if (typeof value === 'number' && !isNaN(value)) {
+                if (type === "+") {
+                    return _money += value;
+                } else if (type === "-") {
+                    return _money -= value;
+                } else {
+                    throw new Error("nieprawidłowy typ działania")
+                }
+            } else {
+                console.log(typeof value);
+                throw new Error("Nieprawidłowa liczba");
+            }
+        }
+    }
+}
+
+// obiekt testowy - przetestowanie metod w konsoli
+// const wallet = new Wallet(200);
+// wallet.getWalletValue()
+// wallet.checkCanPlay(101);
+// wallet.checkCanPlay(301);
+// wallet.changeWallet(50, '+');
+// wallet.changeWallet(70, '-');
+// wallet.changeWallet('70ds', '-');
+// wallet.changeWallet(40, 'tjshj');
